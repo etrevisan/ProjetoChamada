d69d69d563ac8aff90f19beec42968f9bbb6454e
Emilly Trevisan <emillyvivit@gmail.com>
2021-08-18 18:44:18 -0300

    Enviando pasta com arquivos

---
+++ Projeto_Chamada/Projeto_Chamada/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Chamada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cadastrarAlunosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Application.OpenForms.OfType<Form2>().Count() == 0)
+            {
+                Form2 filho2 = new Form2();
+                filho2.MdiParent = this;
+                filho2.Show();
+            }
+
+        }
+
+        private void cadastrarPalestrasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Application.OpenForms.OfType<Form2>().Count() == 0)
+            {
+                Form3 filho3 = new Form3();
+                filho3.MdiParent = this;
+                filho3.Show();
+            }
+        }
+    }
+}
